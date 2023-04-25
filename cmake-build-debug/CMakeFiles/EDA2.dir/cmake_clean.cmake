68077532ddcdc529f2a0246798c55001c35512e3
janpr <jan.prats04@gmail.com>
2023-04-25 15:05:10 +0200

    SESSIÓ 1 COMMIT 1

---
+++ cmake-build-debug/CMakeFiles/EDA2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EDA2.dir/main.c.obj"
+  "CMakeFiles/EDA2.dir/main.c.obj.d"
+  "libEDA2.a"
+  "libEDA2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EDA2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/janpr/CLionProjects/EDA2
+# Install script for directory: D:/JAN/EDA 2 def
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/janpr/CLionProjects/EDA2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/JAN/EDA 2 def/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
