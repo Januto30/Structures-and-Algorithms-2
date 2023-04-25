aa0e5efffe84590b9579242b15f0e84786091d30
janpr <jan.prats04@gmail.com>
2023-04-25 15:18:19 +0200

    SESSIÓ 1 COMMIT 3

--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef EDA2_MAIN_H
 #define EDA2_MAIN_H
 
+///DECLARACIÓ DE FUNCIONS:
 void hello(void);
 
 #endif //EDA2_MAIN_H
---
+++ menu.h
@@ -0,0 +1,8 @@
+#ifndef EDA2_MENU_H
+#define EDA2_MENU_H
+
+///DECLARACIÓ DE FUNCIONS:
+void menu(void);
+
+
+#endif //EDA2_MENU_H
